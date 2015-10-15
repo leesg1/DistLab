bea36270a191864ce9d9078a54a8cf3d33039a15
leesg1 <leesg@tcd.ie>
2015-10-15 14:31:08 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -15,7 +15,7 @@ class Client {
 
 	public static void main(String[] argv) throws Exception {
 		int port = Integer.parseInt(argv[0]);
-		String message = "KILL_SERVICE\n";	// this is the message sent to the server
+		String message = "HELO text\n";	// this is the message sent to the server
 		Socket socket = new Socket("localhost", port);	// initialise the socket
 		DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
 		outToServer.writeBytes(message + "\n"); // send this to the server
