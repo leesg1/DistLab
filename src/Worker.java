b5a956f4bf8dcc52d1ce610777ed70bb81590633
leesg1 <leesg@tcd.ie>
2015-10-26 23:19:15 +0000

    Update Worker.java

--- src/Worker.java
+++ src/Worker.java
@@ -34,8 +34,10 @@ public class Worker implements Runnable {
 	}
 	// print out the ip, port number and the student id
 	private void dealWithHELO(Socket socket,String message) throws UnknownHostException {
-		System.out.println(message + "IP:" + Inet4Address.getLocalHost()+"\nPort:"
-			+socket.getLocalPort()+"\nStudentID:1ee9647a04c08c2a6d5896be1df1d272d50e01606f56790c53f1d406336b1609\n");
+		String s = message + "IP:" + Inet4Address.getLocalHost()+"\nPort:"
+			+socket.getLocalPort()+"\nStudentID:1ee9647a04c08c2a6d5896be1df1d272d50e01606f56790c53f1d406336b1609\n";
+		DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+		outToClient.writeBytes(s);
 	}
 	
 	// close the socket and kill the executor, ending all threads instantly
