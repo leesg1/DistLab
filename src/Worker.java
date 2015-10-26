473b0a2e0ab76c1667559e285b6969c709e26cb0
leesg1 <leesg@tcd.ie>
2015-10-26 23:35:50 +0000

    Update Worker.java

--- src/Worker.java
+++ src/Worker.java
@@ -38,8 +38,17 @@ public class Worker implements Runnable {
 	private void dealWithHELO(Socket socket,String message) throws Exception{
 		String s = message.trim() + "IP:" + Inet4Address.getLocalHost()+"\nPort:"
 			+socket.getLocalPort()+"\nStudentID:1ee9647a04c08c2a6d5896be1df1d272d50e01606f56790c53f1d406336b1609\n";
-		 PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+		
+			try {
+		PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
 		 pw.println(s);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		
+	
 	}
 	
 	// close the socket and kill the executor, ending all threads instantly
