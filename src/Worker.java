2f80003efe8794846090e0bdc798817a21dc57eb
leesg1 <leesg@tcd.ie>
2015-10-26 23:40:35 +0000

    Update Worker.java

--- src/Worker.java
+++ src/Worker.java
@@ -9,7 +9,7 @@ import java.net.UnknownHostException;
 
 public class Worker implements Runnable {
 
-	public Worker(Socket socket) throws IOException, InterruptedException {
+	public Worker(Socket socket) throws IOException, InterruptedException,Exception {
 		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		String message = inFromClient.readLine();	// read message from client
 		//call the function to process difference types of messages
@@ -35,18 +35,13 @@ public class Worker implements Runnable {
 		}
 	}
 	// print out the ip, port number and the student id
-	private void dealWithHELO(Socket socket,String message){
+	private void dealWithHELO(Socket socket,String message) throws Exception{
 		String s = message.trim() + "IP:" + Inet4Address.getLocalHost()+"\nPort:"
 			+socket.getLocalPort()+"\nStudentID:1ee9647a04c08c2a6d5896be1df1d272d50e01606f56790c53f1d406336b1609\n";
-		
-			try {
+
 		PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-		 pw.println(s);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-		
+		pw.println(s);
+
 		
 	
 	}
